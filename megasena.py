9ef74823be095522d6f4524f3f8b8948b888ae93
Sidney <sidneylcarneiro@yahoo.com>
2026-07-18 13:15:20 -0300

    refactor: aplicacao de nomes significativos para variaveis e funcoes

--- megasena.py
+++ megasena.py
@@ -1,33 +1,33 @@
 import random
 
-def megasena(num: list[int], valor: float) -> float:
-    list = [] # válidos
-    for n in num:
-        if n < 1 or n > 60:
+def calcula_premio(numeros_apostados: list[int], premio_total: float) -> float:
+    numeros_validos = []
+    for apostado in numeros_apostados:
+        if apostado < 1 or apostado > 60:
             return 0.0 # inválido
-        if n in list:
+        if apostado in numeros_validos:
             return 0.0 # repetido
-        list.append(n)
+        numeros_validos.append(apostado)
         
-    if len(list) >= 6 and len(list) <= 15:
-        sort = []
+    if len(numeros_validos) >= 6 and len(numeros_validos) <= 15:
+        numeros_sorteados = []
         s = 0
-        while len(sort) < 6:
+        while len(numeros_sorteados) < 6:
             # random.randint(1, 60) é o equivalente ao new Random().nextInt(59) + 1
             s = random.randint(1, 60) 
-            if s not in sort:
-                sort.append(s)
+            if s not in numeros_sorteados:
+                numeros_sorteados.append(s)
         
-        tot = 0
-        for apostado in num:
-            if apostado in sort:
-                tot += 1
+        acertos = 0
+        for apostado in numeros_apostados:
+            if apostado in numeros_sorteados:
+                acertos += 1
                 
-        if tot == 6:
-            return valor # 100%
-        elif tot == 5:
-            return valor * 0.2 # 20%
-        elif tot == 4:
-            return valor * 0.05 # 5%
+        if acertos == 6:
+            return premio_total # 100%
+        elif acertos == 5:
+            return premio_total * 0.2 # 20%
+        elif acertos == 4:
+            return premio_total * 0.05 # 5%
             
     return 0.0
